771c900f3920df0a2d05b870b06caf09b2a19ea8
rs9110 <rs9110@g.rit.edu>
2021-09-29 21:56:17 -0400

    build for production

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React, { useState, useEffect } from 'react';
 
 import Article from './components/Article';
-import firebase from 'firebase/app';
-
-firebase.initializeApp({
-  apiKey: "AIzaSyBh9Xx9qkuyJYvhTzZiTpcgVOkDtffRVyk",
-  authDomain: "react-subreddit.firebaseapp.com",
-  databaseURL: "https://react-subreddit.firebaseio.com",
-  projectId: "react-subreddit",
-  storageBucket: "react-subreddit.appspot.com",
-  messagingSenderId: "617202479053",
-  appId: "1:617202479053:web:b384ec67bcea4913dfde84",
-  measurementId: "G-ZET6FWY7J1"
-})
 
 function App() {
   const [articles, setArticles] = useState([]);
   const [subreddit, setSubreddit] = useState('movies');
 
   useEffect(() => {
-    fetch("https://www.reddit.com/r/" + subreddit + ".json").then(res => {
+    fetch("https://www.reddit.com/r/"+ subreddit +".json").then(res => {
       if (res.status != 200) {
         console.log("Error!")
         return;
